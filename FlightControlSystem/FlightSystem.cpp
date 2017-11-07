00c9eea144f36367b6c193bc622dca90b60ea1f0
Matthew Webster <32935609+b00714308@users.noreply.github.com>
2017-11-07 15:56:53 +0000

    Add files via upload

---
+++ FlightControlSystem/FlightSystem.cpp
@@ -0,0 +1,40 @@
+/*
+* FlightSystem.cpp
+* Version information v0.1
+* Author: Matthew Webster b00714308
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Aircraft.h"
+#include "FlightSystem.h"
+
+void FlightSystem::AddAircraft(std::string flightNumber, std::string airline, std::string aircraftType, std::string gridReference, int groundspeed, int altitude, int heading) {
+	aircraftList_;
+	Aircraft aircraft1(flightNumber, airline, aircraftType, gridReference, groundspeed, altitude, heading);	
+	aircraftList_.push_back(aircraft1);
+	}
+std::vector<Aircraft>ListAllAircraft(std::string flightNumber, std::string airline, std::string aircraftType, std::string gridReference, int groundspeed, int altitude, int heading) {
+
+}
+std::vector<Aircraft> ListAllCruisingAircraft(std::string flightNumber, std::string airline, std::string aircraftType, std::string gridReference, int groundspeed, int altitude, int heading) {
+
+}
+void FlightSystem::RemoveAircraft(std::string flightNumber) {
+
+}
+void ChangeHeading(std::string flightNumber, int heading) {
+
+}
+int FlightSystem::GetHeading(std::string flightNumber) {
+
+}
+void ChangeAltitude(std::string flightNumber, int heading) {
+
+}
+int FlightSystem::GetAltitude(std::string flightNumber) {
+
+}
+int FlightSystem::numAircraftInSector() {
+
+}
